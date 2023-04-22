b49b013f9934071d68c2ab07923cfa097761db49
jakobvial <76407053+JohnJumpoJump@users.noreply.github.com>
2023-04-22 19:13:06 +0200

    Format files

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
 
-import {
-  BaseButton,
-  GoogleSignInButton,
-  InvertedButton,
-} from '../button/button.styles';
+import {BaseButton, GoogleSignInButton, InvertedButton} from '../button/button.styles';
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,24 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
-import { rootReducer } from './root-reducer';
+import {rootReducer} from './root-reducer';
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
+    Boolean
 );
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+    (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
 const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
+    key: 'root',
+    storage,
+    blacklist: ['user']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -26,9 +26,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
+    persistedReducer,
+    undefined,
+    composedEnhancers
 );
 
 export const persistor = persistStore(store);
